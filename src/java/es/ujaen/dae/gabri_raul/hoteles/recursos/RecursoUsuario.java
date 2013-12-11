878e44560ee6af7baea800182b142f7456efeb34
Gabriel Fernández Moral <gabriel.fernandez.moral@gmail.com>
2013-12-11 21:56:38 +0100

    Crear usuarios

--- src/java/es/ujaen/dae/gabri_raul/hoteles/recursos/RecursoUsuario.java
+++ src/java/es/ujaen/dae/gabri_raul/hoteles/recursos/RecursoUsuario.java
@@ -1,12 +1,19 @@
 package es.ujaen.dae.gabri_raul.hoteles.recursos;
 
 import es.ujaen.dae.gabri_raul.hoteles.beans.BeanOperador;
+import es.ujaen.dae.gabri_raul.hoteles.excepciones.UsuarioErrorDatos;
+import es.ujaen.dae.gabri_raul.hoteles.excepciones.UsuarioErrorPersistir;
 import es.ujaen.dae.gabri_raul.hoteles.modelos.Usuario;
+import java.net.URI;
 import java.util.Map;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -25,8 +32,13 @@ public class RecursoUsuario {
     @GET
     @Path("/{dni}")
     @Produces("application/json; charset=utf-8")
-    public Usuario obtenerUsuario(@PathParam("dni") String dni) {
-        return operador.obtenerUsuario(dni);
+    public Response obtenerUsuario(@PathParam("dni") String dni) {
+        Usuario usuario = operador.obtenerUsuario(dni);
+        if(usuario != null){
+            return Response.ok(usuario).build();
+        }else{
+            return Response.status(Status.NOT_FOUND).build();
+        }
     }
     
     @GET
@@ -34,6 +46,25 @@ public class RecursoUsuario {
     @Produces("application/json; charset=utf-8")
     public Map<String, Usuario> listaUsuarios() {
         return operador.listaUsuarios();
+    }
+    
+    @PUT
+    @Path("/nuevo/{dni}")
+    @Consumes("application/json")
+    public Response crearUsuario (@PathParam("dni") String dni, Usuario usuario){
+        if (usuario == null){
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+        
+        if(operador.obtenerUsuario(dni) != null){
+            return Response.status(Status.CONFLICT).build();
+        }
+        try{
+        operador.altaUsuario(usuario.getNombre(), usuario.getDireccion(), usuario.getDni());
+        }catch(UsuarioErrorDatos | UsuarioErrorPersistir e){
+            return Response.status(Status.NOT_ACCEPTABLE).build();
+        }
+        return Response.created(URI.create("")).build();
     }
     
 }
