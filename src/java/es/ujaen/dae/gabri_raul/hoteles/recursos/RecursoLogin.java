a6abcc3f0253eb558b8d399dcac6a69040f0a2ce
Raúl Moya Reyes <raulmoyareyes@hotmail.com>
2013-12-14 17:42:10 +0100

    Cambiando el método a Get para poder solicitar operadores.

--- src/java/es/ujaen/dae/gabri_raul/hoteles/recursos/RecursoLogin.java
+++ src/java/es/ujaen/dae/gabri_raul/hoteles/recursos/RecursoLogin.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package es.ujaen.dae.gabri_raul.hoteles.recursos;
 
 import es.ujaen.dae.gabri_raul.hoteles.beans.BeanOperador;
 import es.ujaen.dae.gabri_raul.hoteles.modelos.Operador;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -18,34 +17,34 @@ import javax.ws.rs.core.Response.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 /**
  * Recurso REST para el Operador
  *
- * @author Raúl &  Gabri
+ * @author Raúl & Gabri
  */
 @Path("/login")
 @Component(value = "recursoLogin")
 public class RecursoLogin {
-    
+
     @Autowired
     BeanOperador operador;
-    
-    @POST
+
+    @GET
     @Path("")
     @Consumes("application/json")
     @Produces("application/json; charset=utf-8")
-    public Response login (@QueryParam("usuario") String usuario, String pass){
-        if (usuario == null){
+    public Response login(@QueryParam("usuario") String usuario, String pass) {
+
+        if (usuario == null) {
             return Response.status(Status.BAD_REQUEST).build();
-        }else{
+        } else {
             Operador op = operador.login(usuario, null);
-            if (op == null){
+            if (op == null) {
                 return Response.status(Status.NOT_FOUND).build();
-            }else{
+            } else {
                 return Response.ok(op).build();
             }
         }
     }
-    
+
 }
