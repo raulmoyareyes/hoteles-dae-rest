1d82e45da028a160ea865ecdd62645045e55ea0e
Raúl Moya Reyes <raulmoyareyes@hotmail.com>
2013-12-14 17:50:57 +0100

    Algunas modificaciones.

--- src/java/es/ujaen/dae/gabri_raul/hoteles/recursos/RecursoUsuario.java
+++ src/java/es/ujaen/dae/gabri_raul/hoteles/recursos/RecursoUsuario.java
@@ -9,7 +9,8 @@ import es.ujaen.dae.gabri_raul.hoteles.excepciones.UsuarioErrorPersistir;
 import es.ujaen.dae.gabri_raul.hoteles.excepciones.UsuarioNoEncontrado;
 import es.ujaen.dae.gabri_raul.hoteles.modelos.Usuario;
 import java.net.URI;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -26,84 +27,84 @@ import org.springframework.stereotype.Component;
 /**
  * Recurso REST para el Operador
  *
- * @author Raúl &  Gabri
+ * @author Raúl & Gabri
  */
 @Path("/usuarios")
 @Component(value = "recursoUsuario")
 public class RecursoUsuario {
-    
+
     @Autowired
     BeanOperador operador;
-    
+
     @GET
     @Path("/{dni}")
     @Produces("application/json; charset=utf-8")
     public Response obtenerUsuario(@PathParam("dni") String dni) {
         Usuario usuario = operador.obtenerUsuario(dni);
-        if(usuario != null){
+        if (usuario != null) {
             return Response.ok(usuario).build();
-        }else{
+        } else {
             return Response.status(Status.NOT_FOUND).build();
         }
     }
-    
+
     @GET
     @Path("")
     @Produces("application/json; charset=utf-8")
-    public Map<String, Usuario> listaUsuarios() {
-        return operador.listaUsuarios();
+    public List<Usuario> listaUsuarios() {
+        return new ArrayList(operador.listaUsuarios().values());
     }
-    
+
     @PUT
     @Path("/{dni}")
     @Consumes("application/json")
-    public Response altaUsuario (@PathParam("dni") String dni, Usuario usuario){
-        if (usuario == null){
+    public Response altaUsuario(@PathParam("dni") String dni, Usuario usuario) {
+        if (usuario == null) {
             return Response.status(Status.BAD_REQUEST).build();
         }
-        
-        if(operador.obtenerUsuario(dni) != null){
+
+        if (operador.obtenerUsuario(dni) != null) {
             return Response.status(Status.CONFLICT).build();
         }
-        try{
-        operador.altaUsuario(usuario.getNombre(), usuario.getDireccion(), usuario.getDni());
-        }catch(UsuarioErrorDatos | UsuarioErrorPersistir e){
+        try {
+            operador.altaUsuario(usuario.getNombre(), usuario.getDireccion(), usuario.getDni());
+        } catch (UsuarioErrorDatos | UsuarioErrorPersistir e) {
             return Response.status(Status.NOT_ACCEPTABLE).build();
         }
         return Response.created(URI.create("")).build();
     }
-    
+
     @DELETE
     @Path("/{dni}")
     //@Consumes("application/json")
-    public Response bajaUsuario (@PathParam("dni") String dni){
+    public Response bajaUsuario(@PathParam("dni") String dni) {
         Usuario usuario = operador.obtenerUsuario(dni);
-        if(usuario == null){
+        if (usuario == null) {
             return Response.status(Status.NOT_FOUND).build();
-        }else{
-            try{
+        } else {
+            try {
                 operador.bajaUsuario(dni);
-            }catch (UsuarioErrorEliminar | UsuarioNoEncontrado | ReservaErrorCambiarUsuario e){
+            } catch (UsuarioErrorEliminar | UsuarioNoEncontrado | ReservaErrorCambiarUsuario e) {
                 return Response.status(Status.NOT_ACCEPTABLE).build();
             }
             return Response.status(Status.ACCEPTED).build();
         }
     }
-    
+
     @POST
     @Path("/{dni}")
     @Consumes("application/json")
-    public Response modificarUsuario (@PathParam("dni") String dni, Usuario usuario){
-        if (usuario == null){
+    public Response modificarUsuario(@PathParam("dni") String dni, Usuario usuario) {
+        if (usuario == null) {
             return Response.status(Status.BAD_REQUEST).build();
         }
-        
-        if(operador.obtenerUsuario(dni) == null){
+
+        if (operador.obtenerUsuario(dni) == null) {
             return Response.status(Status.NOT_FOUND).build();
         }
-        try{
-        operador.modificarUsuario(usuario.getNombre(), usuario.getDni(), usuario.getDireccion());
-        }catch(UsuarioErrorActualizar e){
+        try {
+            operador.modificarUsuario(usuario.getNombre(), usuario.getDni(), usuario.getDireccion());
+        } catch (UsuarioErrorActualizar e) {
             return Response.status(Status.NOT_ACCEPTABLE).build();
         }
         return Response.status(Status.ACCEPTED).build();
